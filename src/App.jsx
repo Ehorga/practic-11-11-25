3ebc97c1bf41fd40284da238c00819f6bb652104
Ehorga <yehornahiba1207@gmail.com>
2025-11-18 15:31:15 +0200

    25-11-17

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import "./App.css";
+import './App.css';
+
+function Heading() {
+  const sum = (n1, n2) => n1 + n2;
+  return <h2>title qwerty {sum(2, 3)}</h2>;
+}
+
+//create paragraf
 
 function App() {
+  const name = 'Anna';
+  const isAdult = true;
   return (
     <>
-      <h1 className="main">Hello world</h1>
+      <h1 className="heading">
+        {isAdult ? 'Hello' : 'Hi'}, {name}!
+      </h1>
+
+      <Heading />
+      {/* paragraf */}
     </>
   );
 }
