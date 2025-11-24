db32c77d2c1f86e06e358367b64480093ab52187
Ehorga <yehornahiba1207@gmail.com>
2025-11-24 23:06:25 +0200

    Practice props

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-import './App.css';
-
-function Heading() {
-  const sum = (n1, n2) => n1 + n2;
-  return <h2>title qwerty {sum(2, 3)}</h2>;
-}
-
-function Paragraf(){
-  return <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, cum necessitatibus harum dignissimos iste repudiandae, ex itaque iusto, velit odio molestias a fugiat numquam quibusdam nihil laboriosam aperiam perspiciatis temporibus.</p>
-}
+import Product from "./components/Product/Product";
+import UserProfile from "./components/UserProfile/UserProfile";
 
 function App() {
-  const name = 'Anna';
-  const isAdult = true;
-  return (
+  return { 
     <>
-      <h1 className="heading">
-        {isAdult ? 'Hello' : 'Hi'}, {name}!
-      </h1>
-
-      <Heading />
-      {/* paragraf */}
-      <Paragraf/>
+  // <UserProfile name = "Yehor" age = "23" email = "yehor@gmail.com"/>
+  <table>
+    <tr>
+      <th>title</th>
+      <th>price</th>
+      <th>quantity</th>
+    </tr>
+    <Product title ="milk" price={32} quantity{1}/>
+       </table>
     </>
-  );
+  };
 }
 
 export default App;
