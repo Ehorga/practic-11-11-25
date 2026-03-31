f78c83fb5dd6622e87472bdec8d4c22a4332eb03
Ehorga <yehornahiba1207@gmail.com>
2026-03-31 15:35:51 +0300

    halfContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import Wrapper from "./components/Wrapper/Wrapper";
-import ModalWindow from "./components/ModalWindow/ModalWindow";
-import { useState, useEffect } from "react";
-import DropDownMenu from "./components/DropDownMenu/DropDownMenu";
-import Tooltip from "./components/Tooltip/Tooltip";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <Wrapper>
-        <Tooltip textTooltip="click button and select">
-          <h2>Dropdown Menu!</h2>
-        </Tooltip>
-
-        <DropDownMenu text="hi, user!">
-          <button>account</button>
-          <button>logout</button>
-        </DropDownMenu>
-
-        <DropDownMenu text="type" widthList="500">
-          <p>Lorem, ipsum dolor.</p>
-          <p>Quo, nesciunt sapiente!</p>
-          <p>Tenetur, dolore sit!</p>
-        </DropDownMenu>
-
-        <DropDownMenu text="links">
-          <a href="#">lorem 1</a>
-          <a href="#">lorem 2</a>
-          <a href="#">lorem 3</a>
-        </DropDownMenu>
-      </Wrapper>
+   <Header/>
+    <HomePage/>
     </>
+      
   );
 }
 
---
+++ src/components/UserProfile/UserProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserProfile = () => {
+    return (
+        <div>
+            user={{ name : 'Anonim', age :100, email : 'anonim@gmail.com' }}
+
+        </div>
+    );
+}
+
+export default UserProfile;
